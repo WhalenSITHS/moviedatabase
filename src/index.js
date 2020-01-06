40c8c14a82ddba4fd70141c9055d881004a9b051
Michael Whalen <michael.whalen@sitechhs.com>
2020-01-06 18:38:41 -0500

    added userse

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 require("./db/mongoose"); //ensures mongoose runs and connects
 const app = express();
 const movieRouter = require("./routers/movies");
+const userRouter = require("./routers/user");
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(movieRouter);
+app.use(userRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
